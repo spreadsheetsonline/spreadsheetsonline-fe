d560eec8783bcc6f96be2d81cc4900d6fdba8146
Cairo Stewart <stewartcairo@gmail.com>
2018-10-13 14:07:06 -0700

    add HttpsRedirect to force https

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/bootstrap-css/black-dashboard.css';
 import App from './components/App';
+import HttpsRedirect from 'react-https-redirect';
 import * as serviceWorker from './serviceWorker';
+import './assets/bootstrap-css/black-dashboard.css';
 import 'bootstrap'
 require('./assets/bootstrap-js/bundle');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HttpsRedirect><App /></HttpsRedirect>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
