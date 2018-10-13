855ee5436e5e57fd99b3efe942408cc61eaf20a2
Cairo Stewart <stewartcairo@gmail.com>
2018-10-13 14:51:35 -0700

    remove import HttpsRedirect from 'react-https-redirect'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import HttpsRedirect from 'react-https-redirect';
 import * as serviceWorker from './serviceWorker';
 import './assets/bootstrap-css/black-dashboard.css';
 import 'bootstrap'
 require('./assets/bootstrap-js/bundle');
 
-ReactDOM.render(<HttpsRedirect><App /></HttpsRedirect>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register();
