7cc4fc37dc61f25fd5f9b1e285733130755ee9f5
Cairo Stewart <stewartcairo@gmail.com>
2018-10-13 13:08:15 -0700

    fixing compile error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/index.css';
 import './assets/bootstrap-css/black-dashboard.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
